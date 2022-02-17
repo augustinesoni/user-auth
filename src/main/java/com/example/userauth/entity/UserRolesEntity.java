35d3e5664332f202854332a36a57d28f76146ec2
Soni Augustine <augustinesoni@yahoo.com>
2022-02-17 13:58:33 +0000

    one to many mapping of entities

--- src/main/java/com/example/userauth/entity/UserRolesEntity.java
+++ src/main/java/com/example/userauth/entity/UserRolesEntity.java
@@ -2,9 +2,7 @@ package com.example.userauth.entity;
 
 import lombok.Data;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -13,5 +11,13 @@ public class UserRolesEntity {
 
     @EmbeddedId
     UserRolesId userRolesId;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY, targetEntity = UserEntity.class)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", updatable = false, insertable = false)
+    UserEntity userEntity;
+
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY, targetEntity = RoleEntity.class)
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id", updatable = false, insertable = false)
+    RoleEntity roleEntity;
 
 }
